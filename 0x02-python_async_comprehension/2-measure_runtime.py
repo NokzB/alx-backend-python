c0252e47202bf112456d29ad4b3b503fb5eefad6
Nokuphila Bhembe <nnblb58@gmail.com>
2024-12-23 09:12:25 +0200

    Python Async Comprehension file upload

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Solution for task 2"""
+import asyncio
+import time
+from importlib import import_module as using
+
+
+async_comprehension = using('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """Runs the async_comprehension 4 times and checks the
+    total run times."""
+    
+    start_time = time.time()
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
+    return time.time() - start_time

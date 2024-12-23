1b4ce1f6536b7dbac221870803e043453e454687
Nokuphila <nnblb58@gmail.com>
2024-12-23 07:17:11 +0000

    removed whitespace

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -11,7 +11,6 @@ async_comprehension = using('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """Runs the async_comprehension 4 times and checks the
     total run times."""
-    
     start_time = time.time()
     await asyncio.gather(*(async_comprehension() for _ in range(4)))
     return time.time() - start_time
